49e6205fd006b9c2dd33e45d7781021a33a1286a
Daniel Partogi <danielpartogi1996@gmail.com>
2019-02-05 16:26:08 +0700

    again

--- index.js
+++ index.js
@@ -1,8 +1,27 @@
 const express = require('express');
+const mongoose = require('mongoose');
+const keys = require('./config/keys');
+const passport = require('passport');
+const cookieSession = require('cookie-session');
+require('./models/User');
 require('./services/passport');
-//const authRoutes = require('./routes/authRoutes');
+
+mongoose.connect(
+   keys.mongoURI
+);
 
 const app = express();
+
+app.use(
+    cookieSession({
+        maxAge: 30 *24 *3600 * 1000,
+        keys: [keys.cookieKey]
+    })
+);
+
+app.use(passport.initialize());
+app.use(passport.session());
+
 
 require('./routes/authRoutes')(app);
 
---
+++ models/User.js
@@ -0,0 +1,9 @@
+const mongoose = require('mongoose');
+const Schema = mongoose.Schema;
+//const { Schema } = mongoose; same as above
+
+const userSchema = new Schema({
+    googleId: String,
+});
+
+mongoose.model('users', userSchema);
